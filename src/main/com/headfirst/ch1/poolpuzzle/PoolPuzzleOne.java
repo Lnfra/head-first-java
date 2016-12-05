a613c7014d4397760d6a35b95e53f26bf2147f97
Angeline Tan <atan009@gmail.com>
2016-12-05 18:22:07 +0800

    Add solution to pool puzzle

---
+++ src/main/com/headfirst/ch1/poolpuzzle/PoolPuzzleOne.java
@@ -0,0 +1,46 @@
+package com.headfirst.ch1.poolpuzzle;
+
+/**
+ * From Chapter 1, page 24
+ */
+public class PoolPuzzleOne {
+  public static void main(String[] args) {
+    int x = 0;
+
+    /**
+     * Trying to get the output:
+     * a noise
+     * annoys
+     * an oyster
+     */
+
+    while(x < 4){
+
+      System.out.print("a");
+
+      if(x < 1){
+        System.out.print(" ");
+      }
+
+      System.out.print("n");
+
+      if( x > 1){
+        System.out.print(" oyster");
+        x = x + 2;
+      }
+
+      if( x == 1 ){
+        System.out.print("noys");
+      }
+
+      if ( x< 1) {
+        System.out.print("oise");
+      }
+
+      System.out.println("");
+      x = x + 1;
+
+    }
+
+  }
+}
