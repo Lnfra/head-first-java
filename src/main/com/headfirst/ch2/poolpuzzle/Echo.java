486c2031caa06d06fe942dd2da4ec9b04ec08f6c
Angeline Tan <atan009@gmail.com>
2016-12-05 23:44:21 +0800

    Chapter 2 pool puzzle

---
+++ src/main/com/headfirst/ch2/poolpuzzle/Echo.java
@@ -0,0 +1,12 @@
+package com.headfirst.ch2.poolpuzzle;
+
+/**
+ * Created by lnfra on 5/12/16.
+ */
+public class Echo {
+  int count = 0;
+
+  void hello(){
+    System.out.println("helloooo... ");
+  }
+}
