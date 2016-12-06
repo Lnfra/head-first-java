2d19e1c34d84fbbb0775b586d600fbc8fef770e6
Angeline Tan <atan009@gmail.com>
2016-12-06 16:33:17 +0800

    Chapter4: Added ElectricGuitar example, showing getter and setters

---
+++ src/main/com/headfirst/ch3/electricguitar/ElectricGuitar.java
@@ -0,0 +1,35 @@
+package com.headfirst.ch3.electricguitar;
+
+/**
+ * Chapter 4, Page 79
+ */
+public class ElectricGuitar {
+
+  String brand;
+  int numOfPickups;
+  boolean rockStarUsesIt;
+
+  public String getBrand() {
+    return brand;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  public int getNumOfPickups() {
+    return numOfPickups;
+  }
+
+  public void setNumOfPickups(int numOfPickups) {
+    this.numOfPickups = numOfPickups;
+  }
+
+  public boolean isRockStarUsesIt() {
+    return rockStarUsesIt;
+  }
+
+  public void setRockStarUsesIt(boolean rockStarUsesIt) {
+    this.rockStarUsesIt = rockStarUsesIt;
+  }
+}
